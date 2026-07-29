a9d5e7d8db49b049b221f349fb99c87d977eb88d
Lloyd <lloydlosardo@yahoo.co.uk>
2026-07-29 18:19:19 +0100

    Added first market module

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from market.market_data import get_status
+
+
 def main():
-	print("=" * 40)
-	print("Sentinel")
-	print("AI Trading Research Platform")
-	print("=" * 40)
+    print("=" * 40)
+    print("Sentinel")
+    print("AI Trading Research Platform")
+    print("=" * 40)
+    print(get_status())
 
 if __name__ == "__main__":
 	main()
