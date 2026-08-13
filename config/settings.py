9eff71ea9b0b5bceec6f67fbb94ebe6ec93f51c0
SENTINEL331 <lloydlosardo@yahoo.co.uk>
2026-08-13 13:12:45 +0100

    Add demo broker readiness settings

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+import os
+
 APP_NAME = "Sentinel"
 VERSION = "v1.0"
 AI_MODEL = "gpt-5"
@@ -16,6 +18,11 @@ DEFAULT_PERIOD = "30d"
 DEFAULT_INTERVAL = "1d"
 BACKTEST_PERIOD = "2y"
 BACKTEST_INTERVAL = "1d"
+
+BROKER_MODE = os.getenv("BROKER_MODE", "demo")
+BROKER_BASE_URL = os.getenv("BROKER_BASE_URL", "")
+BROKER_API_KEY = os.getenv("BROKER_API_KEY", "")
+BROKER_API_SECRET = os.getenv("BROKER_API_SECRET", "")
 
 FEATURE_SET = [
     {
